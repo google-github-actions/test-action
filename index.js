ca2a50054ab76facc14508278d9f6cf929b03b4e
Mike Verbanic <verbanicm@google.com>
2024-09-09 11:41:27 -0400

    feat: new version

--- index.js
+++ index.js
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const version = "2.0.0";
+const version = "2.0.1";
 console.log(`test-action v${version}`);

db135563f2ce86bdbfd8ea2998c4de76213a476c
Mike Verbanic <verbanicm@users.noreply.github.com>
2024-09-09 11:32:58 -0400

    feat: add v1.0.1 (#4)

--- index.js
+++ index.js
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const version = "1.0.0";
+const version = "1.0.1";
 console.log(`test-action v${version}`);
